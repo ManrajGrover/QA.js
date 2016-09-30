ff55cd8bf553fbd6074bb238b9bbbf86220312ee
Manraj Singh <manrajsinghgrover@gmail.com>
2016-09-30 23:04:21 +0530

    Base created

--- src/qa.js
+++ src/qa.js
@@ -1,10 +1,3 @@
-/*
-* @Author: Manraj Singh
-* @Date:   2016-09-18 10:22:31
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-09-18 10:24:05
-*/
-
 (function(root, factory){
     if (typeof define === 'function' && define.amd) {
         define(factory);
@@ -13,12 +6,11 @@
     } else {
         root.qa = factory();
     }
-})(this, function(){
+    })(this, function(){
 
     return function qa(args){
         args = args || {};
 
-        return {
-        };
+        return {};
     };
 });
