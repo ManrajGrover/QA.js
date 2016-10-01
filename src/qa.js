665b9bb79950175922c6431bb20bc7866e696587
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-01 22:00:11 +0530

    Gulp file added

--- src/qa.js
+++ src/qa.js
@@ -1,16 +1,5 @@
-(function(root, factory){
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.qa = factory();
-    }
-    })(this, function(){
-
-    return function qa(args){
-        args = args || {};
-
-        return {};
-    };
-});
+class qa {
+  constructor() {
+    console.log("Babel working");
+  }
+}
